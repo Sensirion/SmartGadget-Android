6b90eec8dccebf6be067c45a1f7c251de0770c02
ahmedmaz24 <ahmed.mediouni@medtech.tn>
2019-04-26 00:08:25 +0100

    Fetching from JSON file

---
+++ app/src/main/java/com/sensirion/smartgadget/view/Fetching.java
@@ -0,0 +1,81 @@
+package com.example.yarabi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+//in this class here is the class where we fetch from a json file but for now its not integrated in the app 
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.jsonfetch);
+
+        mTextViewResult = findViewById(R.id.text_view_result);
+        Button buttonParse = findViewById(R.id.button_parse);
+
+        mQueue = Volley.newRequestQueue(this);
+
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jsonParse();
+            }
+        });
+    }
+
+    private void jsonParse() {
+
+        String url = "https://api.myjson.com/bins/9uh0g";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("employees");
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject employee = jsonArray.getJSONObject(i);
+
+                                String temperature = employee.getString("temperature");
+                                int humidity = employee.getInt("humidity");
+                                String soil = employee.getString("moisture");
+
+                                mTextViewResult.append("\n\n"+"Temperature is :"+temperature + "\n\n\n\n" +"The Humidity is :" + String.valueOf(humidity) + "\n\n\n\n "+"soil moisure is :" + soil + "\n\n");
+                               if (humidity>5){Toast.makeText(getApplicationContext(),"Too humid plant might be infected by funges",Toast.LENGTH_SHORT).show();
+
+
+                            }}
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
+    }
+}
